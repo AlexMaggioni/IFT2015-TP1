d5b25c812c17eb735fd6574d863925f8b0b1ccbf
AlexMaggioni <veralex1000@gmail.com>
2023-10-22 05:34:35 -0400

    Added comments

--- Main.java
+++ Main.java
@@ -4,49 +4,72 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe principale permettant de lire et de traiter les requêtes à partir d'un fichier donné.
+ */
 public class Main {
+    // Liste pour stocker les requêtes
     private final List<Query> queries = new ArrayList<>();
 
+    /**
+     * Constructeur permettant d'initialiser les requêtes à partir d'un fichier.
+     *
+     * @param filename Le nom du fichier à lire.
+     */
     public Main(String filename) {
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = br.readLine()) != null && !line.trim().isEmpty()) {
+                // Découpage de la ligne en parties
                 String[] parts = line.split(" ");
                 int M = Integer.parseInt(parts[0]);
                 int N = Integer.parseInt(parts[1]);
+                // Initialisation de la grille
                 char[][] grid = new char[M][N];
                 for (int i = 0; i < M; i++) {
-                    grid[i] = br.readLine().replace(" ", "").toCharArray();  // Reverted the change here
+                    grid[i] = br.readLine().replace(" ", "").toCharArray();
                 }
+                // Récupération des mots
                 List<String> words = List.of(br.readLine().split(" "));
                 queries.add(new Query(M, N, grid, words));
             }
         } catch (IOException e) {
-            System.err.println("Error reading the file: " + e.getMessage());
+            System.err.println("Erreur lors de la lecture du fichier: " + e.getMessage());
         }
     }
 
+    /**
+     * Méthode pour obtenir la liste des requêtes.
+     *
+     * @return La liste des requêtes.
+     */
     public List<Query> getQueries() {
         return queries;
     }
 
-    public static void main(String[] args) {
+    /**
+     * Point d'entrée principal du programme.
+     *
+     * @param args Les arguments de la ligne de commande.
+     */
+    public static void main(String[] args) {   
         if (args.length < 1) {
-            System.out.println("Please provide the input filename.");
+            System.out.println("Veuillez fournir le nom du fichier d'entrée.");
             return;
         }
         Main reader = new Main(args[0]);
         int queryNumber = 1;
         StringBuilder output = new StringBuilder();
+        // Traitement des requêtes
         for (Query query : reader.getQueries()) {
-            output.append("Query ").append(queryNumber).append("\n");
+            output.append("Requête ").append(queryNumber).append("\n");
             for (String word : query.getWords()) {
-                for (String path : query.findWordPaths(word)) {  // This is now a String
+                for (String path : query.findWordPaths(word)) {
                     output.append(word).append(" ").append(path).append("\n");
                 }
             }
             queryNumber++;
         }        
         System.out.print(output);
-    }
+    }   
 }
--- Query.java
+++ Query.java
@@ -1,5 +1,8 @@
 import java.util.*;
 
+/**
+ * Classe représentant une requête contenant une grille et une liste de mots.
+ */
 public class Query {
     private final int M;
     private final int N;
@@ -8,53 +11,89 @@ public class Query {
     private static final int[] DX = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
     private static final int[] DY = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
 
+    /**
+     * Constructeur pour initialiser une requête.
+     *
+     * @param M      Nombre de lignes de la grille.
+     * @param N      Nombre de colonnes de la grille.
+     * @param grid   Grille de lettres.
+     * @param words  Liste de mots à chercher.
+     */
     public Query(int M, int N, char[][] grid, List<String> words) {
         this.M = M;
         this.N = N;
         this.grid = grid;
+        // Copie et tri des mots
         this.words = new ArrayList<>(words);
         Collections.sort(this.words);
     }
 
+    /**
+     * Méthode pour obtenir la liste des mots.
+     *
+     * @return La liste des mots.
+     */
     public List<String> getWords() {
         return words;
     }
 
+    /**
+     * Trouve les chemins dans la grille pour un mot donné.
+     *
+     * @param word Le mot à chercher.
+     * @return Une liste de chemins où le mot est trouvé.
+     */
     public List<String> findWordPaths(String word) {
         char[] wordChars = word.toCharArray();
         List<String> paths = new ArrayList<>();
+        // Parcourir chaque cellule de la grille
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
+                // Si la première lettre du mot est trouvée
                 if (grid[i][j] == wordChars[0]) {
                     StringBuilder path = new StringBuilder();
                     dfs(i, j, 0, wordChars, path, paths);
                 }
             }
         }
-        Collections.sort(paths);  // Sorting the paths
+        Collections.sort(paths);
         return paths;
     }
-    
 
+    /**
+     * Fonction récursive pour explorer la grille à la recherche d'un mot.
+     *
+     * @param x         Coordonnée x actuelle.
+     * @param y         Coordonnée y actuelle.
+     * @param index     Index actuel dans le mot.
+     * @param wordChars Caractères du mot.
+     * @param path      Chemin actuel.
+     * @param paths     Liste des chemins trouvés.
+     */
     private void dfs(int x, int y, int index, char[] wordChars, StringBuilder path, List<String> paths) {
+        // Vérifier les limites de la grille
         if (x < 0 || x >= M || y < 0 || y >= N) return;
     
         char currentChar = grid[x][y];
+        // Vérifier si le caractère correspond
         if (currentChar != wordChars[index]) return;
     
+        // Mise à jour du chemin
         int pathLength = path.length();
         path.append("(").append(x).append(",").append(y).append(")");
     
+        // Si le dernier caractère du mot est atteint
         if (index == wordChars.length - 1) {
             paths.add(path.toString());  
         } else {
             path.append("->");
+            // Explorer les voisins
             for (int i = 0; i < 9; i++) {
                 dfs(x + DX[i], y + DY[i], index + 1, wordChars, path, paths);
             }
         }
     
-        path.setLength(pathLength); // Revert the path to its previous state
+        // Réinitialiser le chemin pour l'itération suivante
+        path.setLength(pathLength);
     }
-    
 }
